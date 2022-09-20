ced94c243677eca9dcbd42cd6e84e6bb21c7ac49
kabiru <kbruabdullahi@gmail.com>
2022-09-20 03:31:21 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -16,7 +16,13 @@ char *_strcpy(char *dest, char *src)
 	do {
 		i++;
 		dest[i] = src[i];
-	} while (src[i] != '\0');
+	 <= '9')
+		num = num * 10 + (*s - '0');
+	 else if (num > 0)
+		break;
+	} while (*s++);
+       return (num *i);
+}	while (src[i] != '\0');
 
 	return (dest);
 }
